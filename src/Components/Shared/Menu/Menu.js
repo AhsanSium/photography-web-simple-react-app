4962456b25d6410e4b9c9c5cbf6485e277b90fd0
Ahsan Sium <68369475+AhsanSium@users.noreply.github.com>
2021-09-20 19:26:13 +0600

    Fixing Error

--- src/Components/Shared/Menu/Menu.js
+++ src/Components/Shared/Menu/Menu.js
@@ -4,13 +4,13 @@ const Menu = () => {
     return (
         <div className='Menu'>
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Service & Package</a></li>
-                <li><a href="#">Gallery</a></li>
-                <li><a href="#">Package</a></li>
-                <li><a href="#">FAQ</a></li>
-                <li><a href="#">Contact</a></li>
+                <li><a href="/#">Home</a></li>
+                <li><a href="/#">About</a></li>
+                <li><a href="/#">Service & Package</a></li>
+                <li><a href="/#">Gallery</a></li>
+                <li><a href="/#">Package</a></li>
+                <li><a href="/#">FAQ</a></li>
+                <li><a href="/#">Contact</a></li>
             </ul>
         </div>
     );
